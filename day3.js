644cf10150749f61990b1207b571433a97e8f578
Marsa <mgomezlavall@gmail.com>
2024-12-03 23:13:47 +0100

    Day 3 completed

--- day3.js
+++ day3.js
@@ -7,7 +7,28 @@ const parseAndMultiply = (corruptedValue)=>{
     return matchAll.reduce((acc,val)=>{
         return acc + (val[1]*val[2])
     },0)
-
 }
 
-console.log(parseAndMultiply(data));
+
+// This one takes do´s and donts into consideration
+const parseAndMultiplyEnhanced = (corruptedValue)=>{
+    const parsedValue = corruptedValue.matchAll(/mul\((\d+)[,](\d+)\)|do\(\)|don't\(\)/g);
+    const matchAll = Array.from(parsedValue);
+    let isEnabled = true;
+    return matchAll.reduce((acc,val)=>{
+        if(val[0] === "don't()"){
+            isEnabled = false;
+            return acc;
+        }
+        if(val[0] === "do()"){
+            isEnabled = true;
+            return acc;
+        }
+        if(!isEnabled){
+            return acc;
+        }
+        return acc + (val[1]*val[2])
+    },0)
+}
+
+console.log(parseAndMultiplyEnhanced(data));
